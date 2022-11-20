62229cf6f5bf9ddac2410ca09ad4af8d6410eb19
Imdadul Haque <45633928+imdadulhaque1@users.noreply.github.com>
2022-11-20 14:10:13 +0600

    Trying to remove zero infront of number

--- 191.NumberOf_1_Bits.js
+++ 191.NumberOf_1_Bits.js
@@ -1,18 +1,21 @@
-let n = 00000000000000000000000000001011;
+let n = 00010110;
+
+// console.log("00010110".replace(/^0+/, ''))
+console.log(padStart(n))
+
 
 var hammingWeight = function(n) {
+    // let explitZero = BigInt(n);
     let count = 0;
     const splitNum = [...String(n)].map(Number)
     for(let i=0; i<splitNum.length; i++){
-        console.log(splitNum[i])
+        console.log(splitNum[i]) 
         if(splitNum[i] == 1){
             count += 1
             console.log(splitNum[i])
         }
     }
     return count;
-    
-
 };
 console.log(hammingWeight(n));
 
